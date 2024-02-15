1dfbcc80554cca4d44c946cb4d3da925fee7e0a1
zeecov <isaacogunmola@gmail.com>
2024-02-15 19:07:34 +0000

    Lets fck this up

--- webapp/src/main/webapp/script.js
+++ webapp/src/main/webapp/script.js
@@ -1,6 +1,7 @@
-function displayGreeting() {
-    var name = document.getElementById('name').value;
-    var greetingElement = document.getElementById('greeting');
-    greetingElement.textContent = 'Hello, ' + name + '! Welcome to the Personalized Greeting App.';
-}
+/*
+Created by Alex Devero
+More information on 
+http://alexdevero.com
 
+To learn about HTML5, CSS3 and Javascript visit http://alexsblog.org
+*/
